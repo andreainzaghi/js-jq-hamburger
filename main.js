80a4950b2f545ce57ad432674377c236b456aeae
Andrea <andrea3aprile99@gmail.com>
2021-03-11 15:28:00 +0100

    inizio esercizio jquery

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideDown("slow");
+  });
+});
+
+
+$(document).ready(function(){
+  $("#ics").click(function(){
+    $("#panel").slideUp("fast");
+  });
+});
